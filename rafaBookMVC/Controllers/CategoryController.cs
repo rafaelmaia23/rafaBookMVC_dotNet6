7cd37b94bb935861bcb6ac39fe0f26f83b25d62e
rafaelmaia23 <rafaelmaiadafonseca@gmail.com>
2022-05-06 16:21:17 -0300

    Add Model Category, Index Metod and view. Added DbContex
    Add Model Category, Index Metod and view. Added DbContex

---
+++ rafaBookMVC/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using rafaBookMVC.Data;
+
+namespace rafaBookMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public CategoryController(AppDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}
---
+++ rafaBookMVC/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using rafaBookMVC.Models;
+
+namespace rafaBookMVC.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; } 
+    }
+}
